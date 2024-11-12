570c283816a209154dde498d33ed1b42c4a754c9
sbabadag <cesbabadag@gmail.com>
2024-11-12 18:10:58 +0300

    First Commit

---
+++ app/users/[id].tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+const UserPage = () => {
+    return (
+        <View>
+            <Text>User Page</Text>
+        </View>
+    );
+};
+
+export default UserPage;
